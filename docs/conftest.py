b99f3013a3287c54d103e9cba3e4f4a10c0c6b45
Michael Goerz <goerz@stanford.edu>
2018-11-17 12:02:36 -0500

    Added conftest with a workaround

---
+++ docs/conftest.py
@@ -0,0 +1,13 @@
+"""This file is automatically executed by pytest when testing anything in the
+docs folder"""
+
+
+def pytest_collectstart(collector):
+    """Ignore stderr and javascript output when verifying notebooks.
+
+    Works around a test failure on Travis/AppVeyor (See Travis Build 56).
+
+    https://nbval.readthedocs.io/en/latest/#Skipping-certain-output-types
+    """
+    if collector.__class__.__name__ == 'IPyNbFile':
+        collector.skip_compare += 'application/javascript', 'stderr',
