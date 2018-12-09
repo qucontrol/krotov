333112223bc3670806aea509a61bfe12a6c98d7f
Michael Goerz <goerz@stanford.edu>
2018-12-09 14:45:02 -0500

    Remove Result from top-level public API
    A user doesn't have to instantiate a Result manually, or subclass from
    it, so it shouldn't be exposed by the top level API

--- src/krotov/__init__.py
+++ src/krotov/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.0.1'
 
-# expose submodules for easy inte
+# expose submodules for easy import
 from . import shapes
 from . import structural_conversions
 from . import propagators
@@ -9,13 +9,13 @@ from . import parallelization
 from . import info_hooks
 from . import objectives
 from . import mu
+from . import result
 
 # expose primary classes/functions
 from .objectives import Objective, gate_objectives
 from .pulse_options import PulseOptions
-from .result import Result
 from .optimize import optimize_pulses
 
 __all__ = [
-    'Result', 'Objective', 'PulseOptions', 'gate_objectives',
+    'Objective', 'PulseOptions', 'gate_objectives',
     'optimize_pulses']
