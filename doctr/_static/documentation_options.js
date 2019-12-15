0eb0b8d62ea5174a164b44a7448512e39bf21da9
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-12-15 05:52:34 +0000

    Update docs after building Travis build 627 of qucontrol/krotov
    The docs were built from the branch 'doctr' against the commit
    0da9cb67ce4bf7dac13d2f56bafe3ba98b6ab3b1.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/qucontrol/krotov/jobs/625211036.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/lib/python3.7/site-packages/doctr/__main__.py deploy --key-path docs/doctr_deploy_key.enc '--command=git show 0da9cb67ce4bf7dac13d2f56bafe3ba98b6ab3b1:.travis/docs_post_process.py > post_process.py && git show 0da9cb67ce4bf7dac13d2f56bafe3ba98b6ab3b1:.travis/versions.py > versions.py && python post_process.py' --built-docs docs/_build --no-require-master --build-tags doctr

--- doctr/_static/documentation_options.js
+++ doctr/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0-dev (9f4209b)',
+    VERSION: '1.0.0-dev (0da9cb6)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
