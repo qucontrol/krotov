3879e1e728cd8f5a177e1ecb98ab2d1aa700ac0c
Michael Goerz <goerz@stanford.edu>
2020-01-02 02:55:48 -0500

    Add codecov as dev-requirement

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ requirements = ['glom', 'numpy', 'scipy', 'qutip>=4.3.1', 'uniseg']
 dev_requirements = [
     'better-apidoc==0.3.1',
     'click',
+    'codecov',
     'coverage<5.0',
     # https://github.com/computationalmodelling/nbval/issues/129
     'flake8',
