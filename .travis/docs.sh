b2f9cb401ef8c3c62b0d4e72979b11445f2c13fe
Michael Goerz <goerz@stanford.edu>
2020-03-14 14:44:39 -0400

    Use doctr-versions-menu package

--- .travis/docs.sh
+++ .travis/docs.sh
@@ -3,39 +3,36 @@ echo "## Generate main html documentation"
 tox -c tox-pyenv.ini -e docs
 
 if [ ! -z "$TRAVIS_TAG" ]; then
-    echo "Deploying as TAG $TRAVIS_TAG"
-    echo "## Generate documentation downloads"
-    mkdir docs/_build/download
-    echo "### [htmlzip]"
-    tox -c tox-pyenv.ini -e docs -- -b html _build_htmlzip
-    cd docs || exit
-    mv  _build_htmlzip krotov.html
-    zip -r krotov.html.zip ./krotov.html
-    cd ../ || exit
-    mv docs/krotov.html.zip docs/_build/download
+    echo "Building as tag '$TRAVIS_TAG'"
+elif [ ! -z "$TRAVIS_BRANCH" ]; then
+    echo "Building as branch '$TRAVIS_BRANCH'"
 else
-    echo "Deploying as BRANCH $TRAVIS_BRANCH"
+    echo "At least one of TRAVIS_TAG and TRAVIS_BRANCH must be set"
+    sync
+    exit 1
 fi
 
 
 # Deploy
-if [ ! -z "$TRAVIS" ]; then
+if [ ! -z "$TRAVIS" ] && [ "$TRAVIS_EVENT_TYPE" != "pull_request" ]; then
     echo "## pip install doctr"
-    python -m pip install doctr
+    python -m pip install doctr-versions-menu
     echo "## doctr deploy"
     if [ ! -z "$TRAVIS_TAG" ]; then
         DEPLOY_DIR="$TRAVIS_TAG"
     else
         case "$TRAVIS_BRANCH" in
-            master) DEPLOY_DIR="$TRAVIS_BRANCH";;
+            master|doctr) DEPLOY_DIR="$TRAVIS_BRANCH";;
             *)      echo "Not deploying branch $TRAVIS_BRANCH (not in whitelist)";;
         esac
     fi
     if [ ! -z "$DEPLOY_DIR" ]; then
         python -m doctr deploy --key-path docs/doctr_deploy_key.enc \
-            --command="git show $TRAVIS_COMMIT:.travis/docs_post_process.py > post_process.py && git show $TRAVIS_COMMIT:.travis/versions.py > versions.py && python post_process.py" \
+            --command=doctr-versions-menu \
             --built-docs docs/_build --no-require-master --build-tags "$DEPLOY_DIR"
     fi
+else
+    echo "Not deploying to gh-pages (PR or not on Travis)"
 fi
 
 echo "# DOCTR - DONE"
