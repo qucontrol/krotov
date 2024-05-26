89daf0b220140ac32120f8658c9fd1e8bf16345b
Boxi Li <etamin1201@gmail.com>
2024-05-26 13:04:32 +0200

    Update the dumped test file

--- tests/test_dump_result.py
+++ tests/test_dump_result.py
@@ -5,6 +5,12 @@ import os
 import krotov
 import pytest
 
+import qutip
+from packaging.version import parse as parse_version
+if parse_version(qutip.__version__) < parse_version("5"):
+    oct_result_name = "oct_result_qutip4.dump"
+else:
+    oct_result_name = "oct_result_qutip5.dump"
 
 def incl_range(a, b, step=1):
     e = 1 if step > 0 else -1
@@ -17,39 +23,41 @@ def incl_range(a, b, step=1):
 
 
 @pytest.fixture
-def oct_result_20(request):
+def oct_result_18(request):
     testdir = os.path.splitext(request.module.__file__)[0]
-    filename = os.path.join(testdir, "oct_result.dump")
+    filename = os.path.join(testdir, oct_result_name)
     return krotov.result.Result.load(filename)
 
 
 @pytest.fixture
-def oct_result_5(oct_result_20):
-    result = copy.deepcopy(oct_result_20)
+def oct_result_5(oct_result_18):
+    result = copy.deepcopy(oct_result_18)
     result.iters = list(incl_range(0, 5))
     return result
 
 
 @pytest.fixture
-def oct_result_6(oct_result_20):
-    result = copy.deepcopy(oct_result_20)
+def oct_result_6(oct_result_18):
+    result = copy.deepcopy(oct_result_18)
     result.iters = list(incl_range(0, 6))
     return result
 
 
 @pytest.fixture
-def oct_result_10(oct_result_20):
-    result = copy.deepcopy(oct_result_20)
+def oct_result_10(oct_result_18):
+    result = copy.deepcopy(oct_result_18)
     result.iters = list(incl_range(0, 10))
     return result
 
 
-def test_invalid_dump_result(oct_result_20):
+def test_invalid_dump_result(oct_result_18):
     with pytest.raises(ValueError):
-        krotov.convergence.dump_result('oct_result.dump', every=0)
+        krotov.convergence.dump_result(oct_result_name, every=0)
     filename = "non_existent_folder/oct_result.dump"
-    dump_result = krotov.convergence.dump_result(filename)
-    response = dump_result(oct_result_20)
+    # Use every=18 because this result only has 18 iterations.
+    # The default every=10 returns None.
+    dump_result = krotov.convergence.dump_result(filename, every=18)
+    response = dump_result(oct_result_18)
     assert "Could not store" in response
 
 
